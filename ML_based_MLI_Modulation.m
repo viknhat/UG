b1564f0ba2256b63bbb31e819a3e34bb5dadd15f
viknhat <89339788+viknhat@users.noreply.github.com>
2021-08-22 22:56:39 +0530

    Update ML_based_MLI_Modulation.m

--- ML_based_MLI_Modulation.m
+++ ML_based_MLI_Modulation.m
@@ -3,18 +3,18 @@
 % Firing Angles
 
 s=[]; %Segment size
-s(1)=0.5;
-s(2)=0.6;
-s(3)=0.7;
-s(4)=0.8;
-s(5)=0.9;
+s(1)=2;
+s(2)=0.1;
+s(3)=0.1;
+s(4)=0.1;
+s(5)=0.1;
 D=[]; %Duty Cycle
 d=[]; %delay time
-
+val=29;
 
 for i=1:5
-    d(i)=0.01*s(i)/pi/2*1000;
-    D(i)=(pi-s(i))/pi*100;
+    d(i)=0.01*s(i)/sum(s)/2*1000;
+    D(i)=(sum(s)-s(i))/sum(s)*100;
 end
 
 % Gradient Descent
@@ -22,9 +22,11 @@ end
 % Obtaining THD from simulation
 FFTDATA = power_fftscope(ans.Vout);
 FFTDATA = power_fftscope(FFTDATA);
-prev = FFTDATA.THD
+prev = FFTDATA.THD;
 count=0;
-prevS=s(1);
+for i=1:5
+    prevS(i)=s(i);
+end
 
 for i=1:5
     while(1)
@@ -37,21 +39,27 @@ for i=1:5
         FFTDATA = power_fftscope(FFTDATA);
 
         thd = FFTDATA.THD
-        diff = (thd-29)*0.005;
+        diff = (thd-val)*0.005;
         %if(diff<0.001)
         %   break
         %end
         %i=1;
         %for i=1:5
+        
         s(i)=s(i)-diff;
+        for j=i+1:4
+            s(j)=s(j)+diff/(5-i);
+        end
+        
         if thd>prev
-            s(i)=prevS;
+            s(i)=prevS(i);
+            val=val-5;
             break
         end
         prev=thd;
-        prevS=s(i);
-        d(i)=0.01*s(i)/pi/2*1000;
-        D(i)=(pi-s(i))/pi*100;
+        prevS(i)=s(i);
+        d(i)=0.01*s(i)/sum(s)/2*1000;
+        D(i)=(sum(s)-s(i))/sum(s)*100;
        % end
 
         % A2=A2+d2*diff;
