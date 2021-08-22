1d7ce2e76f5c280de3359dc6f7d00a576a058691
viknhat <89339788+viknhat@users.noreply.github.com>
2021-08-22 18:42:06 +0530

    Update ML_based_MLI_Modulation.m

--- ML_based_MLI_Modulation.m
+++ ML_based_MLI_Modulation.m
@@ -18,34 +18,48 @@ for i=1:5
 end
 
 % Gradient Descent
-
+ sim('Modified11levelHbridgeALGO',0.02);
+% Obtaining THD from simulation
+FFTDATA = power_fftscope(ans.Vout);
+FFTDATA = power_fftscope(FFTDATA);
+prev = FFTDATA.THD
 count=0;
-while(count<100)
-    count=count+1;
+prevS=s(1);
 
-    sim('Modified11levelHbridgeALGO',0.02);
-    % Obtaining THD from simulation
+for i=1:5
+    while(1)
+        %count=count+1;
 
-    FFTDATA = power_fftscope(ans.Vout);
-    FFTDATA = power_fftscope(FFTDATA);
+        sim('Modified11levelHbridgeALGO',0.02);
+        % Obtaining THD from simulation
 
-    thd = FFTDATA.THD
-    diff = (thd-29)*0.05;
-    if(diff<0.001)
-        break
+        FFTDATA = power_fftscope(ans.Vout);
+        FFTDATA = power_fftscope(FFTDATA);
+
+        thd = FFTDATA.THD
+        diff = (thd-29)*0.005;
+        %if(diff<0.001)
+        %   break
+        %end
+        %i=1;
+        %for i=1:5
+        s(i)=s(i)-diff;
+        if thd>prev
+            s(i)=prevS;
+            break
+        end
+        prev=thd;
+        prevS=s(i);
+        d(i)=0.01*s(i)/pi/2*1000;
+        D(i)=(pi-s(i))/pi*100;
+       % end
+
+        % A2=A2+d2*diff;
+        % A3=A3+d3*diff;
+        % A4=A4+d4*diff;
+        % A5=A5+d5*diff;
+
     end
-    i=1;
-    %for i=1:5
-    s(i)=s(i)-diff;
-    d(i)=0.01*s(i)/pi/2*1000;
-    D(i)=(pi-s(i))/pi*100;
-   % end
-    
-    % A2=A2+diff;
-    % A3=A3+diff;
-    % A4=A4+diff;
-    % A5=A5+diff;
 end
-
 
 
